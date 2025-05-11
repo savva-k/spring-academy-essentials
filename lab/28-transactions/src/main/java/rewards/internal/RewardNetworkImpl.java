c5cea2a33234cfafb2dfeb1506f31879cb40bb23
Savva Kodeikin <savva.kodeikin@gmail.com>
2025-05-11 18:29:18 +0200

    Lab 7

--- lab/28-transactions/src/main/java/rewards/internal/RewardNetworkImpl.java
+++ lab/28-transactions/src/main/java/rewards/internal/RewardNetworkImpl.java
@@ -1,5 +1,6 @@
 package rewards.internal;
 
+import org.springframework.transaction.annotation.Transactional;
 import rewards.AccountContribution;
 import rewards.Dining;
 import rewards.RewardConfirmation;
@@ -53,7 +54,8 @@ public class RewardNetworkImpl implements RewardNetwork {
 	
 	// TODO-01: Annotate this method as needing transactional behavior
 	// Make sure to use the annotation from Spring not from Java EE.
-	
+
+	@Transactional
 	public RewardConfirmation rewardAccountFor(Dining dining) {
 		Account account = accountRepository.findByCreditCard(dining.getCreditCardNumber());
 		Restaurant restaurant = restaurantRepository.findByMerchantNumber(dining.getMerchantNumber());
